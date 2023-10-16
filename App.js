77871f260ac7e3f23c82efe2b7f1a179cc08fe2b
Wojanix <wojtek.burek@gmail.com>
2023-10-16 17:05:28 +0200

    HEHE Wojanix

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Just checking quick fix</Text>
+      <Text>Just checking quick fixd</Text>
       <StatusBar style="auto" />
     </View>
   );
